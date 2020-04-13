ce5c1367be97d79f0e5cd164a7ea9649a510c716
williamrackley <willrackley65@gmail.com>
2020-04-13 12:22:37 -0400

    scraping state and country info

--- routes/scrape.js
+++ routes/scrape.js
@@ -8,19 +8,59 @@ router.get("/usa", function(req, res) {
 
     axios.get("https://www.worldometers.info/coronavirus/country/us/").then(function(response) {
      
-        // var $ = cheerio.load(response.data);
+        var $ = cheerio.load(response.data);
 
-        // $('div[id="maincounter-wrap"]').each(function(i, element) {
-        //     var result = {};
+        $('div[id="maincounter-wrap"]').each(function(i, element) {
+            var result = {};
 
-        //     result.totalCases = $(element).find("div.maincounter-number").children("span")
-        //   .text().trim();
+            result.totalCases = $(element).find("div.maincounter-number").children("span")
+          .text().trim();
 
-        //   console.log('yo')
-        // })
-        console.log(response)
-
-})
-
-
+          console.log(result.totalCases)
+        })
+    })
 });
+
+//router.get('/states', function(req, res) {
+    axios.get("https://www.worldometers.info/coronavirus/country/us/").then(function(response) {
+     
+        var $ = cheerio.load(response.data);
+
+        $('table[id="usa_table_countries_today"]').each(function(i, element) {
+            let result = {};
+
+            let statesObject = {}
+
+            let statesArray = []
+            // result.states = $(element).find("tbody:nth-child(2)").children('tr:nth-child(3)').children('td:nth-child(1)')
+            // .text().trim();
+
+            for (i=2; i < 53; i++) {
+
+                result.stateName = $(element).find(`tbody:nth-child(2)`).children(`tr:nth-child(${i})`).children('td:nth-child(1)')
+                .text().trim();
+    
+                result.stateTotalCases = $(element).find(`tbody:nth-child(2)`).children(`tr:nth-child(${i})`).children('td:nth-child(2)')
+                .text().trim();
+    
+                result.stateTotalDeaths = $(element).find(`tbody:nth-child(2)`).children(`tr:nth-child(${i})`).children('td:nth-child(4)')
+                .text().trim();
+                
+                statesObject = {
+                    state: result.stateName,
+                    totalCases: result.stateTotalCases,
+                    totalDeaths: result.stateTotalDeaths
+                }
+                
+                statesArray.push(statesObject)
+            }
+
+            
+
+          console.log(statesArray)
+          
+        })
+    })
+
+
+//});
